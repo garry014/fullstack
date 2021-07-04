39afd4e3d5d5911c91396c8ec95ab21a748c8c6e
sekki2323 <sekkiyukine1000@gmail.com>
2021-07-04 17:07:29 +0800

    bug fix customer.js

--- routes/customer.js
+++ routes/customer.js
@@ -23,7 +23,6 @@ const { v4: uuidv4 } = require('uuid');
 const validator = require("email-validator");
 const Regex = require("regex");
 const { username } = require('../config/db');
-const { username } = require('../config/db');
 const regex = /^(?=.[a-z])(?=.[A-Z])(?=.\d)(?=.[@$!%?&])[A-Za-z\d@$!%?&]{8,}$/;
 
 // customer: login page 
